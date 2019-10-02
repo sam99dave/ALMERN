e13184b357db8495dd74d79422c8d7353af4fd67
Samuel <sam99dave@gmail.com>
2019-10-02 20:46:41 +0530

    login & Registration Done Mongo connectivity issues to be solved

---
+++ routes/api/alumini.js
@@ -0,0 +1,64 @@
+const express = require("express");
+
+const router = express.Router();
+
+const validateLoginInput = require("../../validation/login");
+const validateRegisterInput = require("../../validation/register");
+
+const User = require("../../models/User");
+
+// @route GET api/alumini/register
+router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(404).json(req.body);
+  }
+
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = "Email Already Exists";
+      return res.status(404).json(errors);
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      newUser
+        .save()
+        .then(user => res.send("User Added"))
+        .catch(err => console.log(err));
+    }
+  });
+});
+
+// @route GET api/alumini/login
+router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      errors.email = "User not found";
+      return res.status(404).json(errors);
+    }
+
+    // Check password
+    if (password != user.password) {
+      errors.password = "Password Doesnt Match";
+      return res.status(404).json(errors);
+    }
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+mongoose.connect("mongodb://localhost:27017/userdb", { useNewUrlParser: true });
+
+let db = mongoose.connection;
+
+// Check connection
+db.once("open", (req, res) => {
+  console.log("Connected to MongoDB");
+});
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Set a static folder
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/api/alumini", require("./routes/api/alumini"));
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,9 +28,5 @@ const PORT = process.env.PORT || 5000;
 /*app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });*/
-
-// Set a static folder
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/api/login", require("./routes/api/login"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
